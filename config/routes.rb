07dd9436f8a7ce1fbf0dcfefdb8aa0d520a2a58d
Victor Parames <vparames2012@gmail.com>
2025-03-17 16:05:46 -0300

    Homepage done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  get "search", to: "home#search", as: :search 
   get "up" => "rails/health#show", as: :rails_health_check
 end
